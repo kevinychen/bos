435ac8c3a0a1421f1a90b9d4e11a998a60ff165f
kevinychen <binonunquineist@gmail.com>
2014-11-09 15:33:20 -0500

    Spawn processes.

--- kern/syscall.c
+++ kern/syscall.c
@@ -116,10 +116,14 @@ sys_env_set_status(envid_t envid, int status)
 static int
 sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 {
-	// LAB 5: Your code here.
-	// Remember to check whether the user has supplied us with a good
-	// address!
-	panic("sys_env_set_trapframe not implemented");
+	struct Env *env_store;
+    if (envid2env(envid, &env_store, 1) < 0)
+        return -E_BAD_ENV;
+
+    env_store->env_tf = *tf;
+    env_store->env_tf.tf_cs |= 3;
+    env_store->env_tf.tf_eflags |= FL_IF;
+    return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -379,6 +383,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             return sys_exofork();
         case SYS_env_set_status:
             return sys_env_set_status((envid_t) a1, (int) a2);
+        case SYS_env_set_trapframe:
+            return sys_env_set_trapframe((envid_t) a1, (struct Trapframe*) a2);
         case SYS_env_set_pgfault_upcall:
             return sys_env_set_pgfault_upcall((envid_t) a1, (void*) a2);
         case SYS_yield:
