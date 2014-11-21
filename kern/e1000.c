7efa38d98d980b9040f3f3414c64313bcb7dbf6d
kevinychen <binonunquineist@gmail.com>
2014-11-20 22:16:18 -0500

    Create virtual memory mapping for E1000.

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,17 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
+
+volatile uint32_t *e1000_mem;
 
 // Initialize the E1000
 int
 e1000_attachfn(struct pci_func *pcif) {
+    // Enable the E1000
     pci_func_enable(pcif);
+
+    // Allocate memory mapping
+    e1000_mem = (uint32_t*) mmio_map_region(
+            pcif->reg_base[0], pcif->reg_size[0]);
+
     return 0;
 }
