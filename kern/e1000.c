991600e9828cc729a60e19e4d0e04e6de482442f
kevinychen <binonunquineist@gmail.com>
2014-11-21 00:18:08 -0500

    Initialize transmit descriptor array.

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,6 +2,7 @@
 #include <kern/pmap.h>
 
 volatile uint32_t *e1000_mem;
+struct tx_desc transmit_descriptors[NUM_TX];
 
 // Initialize the E1000
 int
@@ -12,6 +13,14 @@ e1000_attachfn(struct pci_func *pcif) {
     // Allocate memory mapping
     e1000_mem = (uint32_t*) mmio_map_region(
             pcif->reg_base[0], pcif->reg_size[0]);
+
+    // Initialize transmit descriptor array
+    e1000_mem[TX_TDBAL >> 2] = PADDR(transmit_descriptors);
+    e1000_mem[TX_TDLEN >> 2] = NUM_TX * sizeof(struct tx_desc);
+    e1000_mem[TX_TDH >> 2] = 0;
+    e1000_mem[TX_TDT >> 2] = 0;
+    e1000_mem[TX_TCTL >> 2] =
+        (1 << TX_TCTL_EN) | (1 << TX_TCTL_PSP) | (0x40 << TX_TCTL_COLD);
 
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,7 +1,30 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#define TX_TCTL		0x00400	// Transmit control register
+#define TX_TCTL_EN	1		// Transmit control enable bit
+#define TX_TCTL_PSP	3		// Transmit control pad short packets bit
+#define TX_TCTL_COLD	12		// Transmit control collision threshold
+#define TX_TIPG		0x00410	// Transmit IPG register
+
+#define TX_TDBAL	0x03800	// Transmit descriptor base address
+#define TX_TDLEN	0x03808	// Transmit descriptor length
+#define TX_TDH		0x03810	// Transmit descriptor head
+#define TX_TDT		0x03818	// Transmit descriptor tail
+
+#define NUM_TX		64		// Number of transmit descriptors
+
 #include <kern/pci.h>
+
+struct tx_desc {
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+};
 
 int
 e1000_attachfn(struct pci_func *pcif);
